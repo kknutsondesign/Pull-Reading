67407462c8c27a771c022eb922a6e77f4aa04e39
Kevin Knutson <nivek1892@gmail.com>
2025-07-27 20:31:37 -0500

    Initial commit

---
+++ progresses.py
@@ -0,0 +1,199 @@
+# Pull all reading progress from the /me endpoint and store as a file
+# Look up every item to pull full book data
+# Export as Goodreads csv
+
+# TO USE:
+# Paste URL to the ABS server in ABS_HOST
+# Paste your api key in the API_KEY - Login to website and look for `Authorization: Bearer {your key}` in your browser's network tab
+# -H and -k flags overwrite hardcoded values
+# -p pulls reading data again even in progress.json exists
+# -t runs like normal but does not write any values
+
+import json
+import os
+import shutil
+import sqlite3
+import requests
+import argparse
+
+# TODO: Options for using db if access is available for different steps
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-H","--host", action='store',help="ABS host, overwrites hardcoded host.")
+parser.add_argument("-k","--key", action='store',help="API key, overwrites hardcoded key.")
+parser.add_argument("-p", "--pull", action='store_true',help="Pull new reading history from the host.")
+parser.add_argument("-t", "--test", action='store_true',help="Run script but do not do any file manipulation.")
+args = parser.parse_args()
+
+# ##### Configuration #####
+
+ABS_HOST = "http://localhost:13378"
+API_KEY = ""
+
+if args.host:
+    ABS_HOST = args.host
+
+if args.key:
+    API_KEY = args.key
+
+ME_URI = "/api/me"
+USER_URI = "/api/user/"
+ITEM_URI = "/api/items/"
+
+AUTH = f"token={API_KEY}"
+# Some way to do metadata-object path too?
+
+##### Methods #####
+
+def pullProgress():
+    print("Pulling progress data")
+    me = requests.get(f"{ABS_HOST}{ME_URI}?token={API_KEY}")
+    print(me.reason)
+
+
+    books = []
+
+    for pObject in me.json()["mediaProgress"]:
+        book = {}
+        book["progress"] = pObject
+        books.append(book)
+
+    print("Pulling book info")
+
+    for book in books:
+        req = requests.get(f"{ABS_HOST}{ITEM_URI}{book["progress"]["libraryItemId"]}?{AUTH}")
+        print(req.reason)
+        info = req.json()
+        if req.ok:
+            book.update(info)
+
+    with open("progress.json","w") as f:
+        if not args.test: 
+            f.write(json.dumps(books))
+        f.close()
+    
+    print("Done pulling reading history")
+
+## Export Types ##
+
+def goodreadsCSV():
+    print("Writing to CSV compatible with GoodReads")
+    # Title, Author, Additional Authors(, seperated list), ISBN, Publisher, Binding?(Audiobook), Year Published, Bookshelves, Bookshelves with positions? (Collections)
+    books = []
+
+    with open("progress.json","r") as js:
+        books = json.load(js)
+        js.close()
+    
+    with open("toGoodreads.csv","w") as csv:
+        headers = ["Title", "Author", "Additional Authors", "ISBN", "Publisher", "Year Published", "Binding"]
+
+        if not args.test:
+            csv.writelines(','.join(f'"{w}"' for w in headers)+'\n')
+        
+        for b in books:
+            
+            Title = ""
+            Authors = []
+            Narrators = []
+            ISBN = ""
+            Publisher = ""
+            Published = ""
+            Binding = ""
+            Tags = ""
+
+            Title = b["media"]["metadata"]["title"]
+            for a in b["media"]["metadata"]["authors"]:
+                Authors.append(a["name"])
+            
+            Narrators = b["media"]["metadata"]["narrators"]
+            ISBN = b["media"]["metadata"]["isbn"]
+            Publisher = b["media"]["metadata"]["publisher"]
+            Published = b["media"]["metadata"]["publishedYear"]
+            Binding = "Audiobook" if b["progress"]["progress"] != 0 and b["progress"]["ebookProgress"] == 0 else ""
+
+            values = []
+            values.append(Title)
+            values.append(Authors[0])
+            values.append(f"{','.join(Authors[1:]+Narrators)}")
+            values.append(ISBN)
+            values.append(Publisher)
+            values.append(Published)
+            values.append(Binding)
+
+            if not args.test:
+                csv.writelines(','.join(f'"{v.replace('"',"'") if v else ""}"' for v in values)+"\n")
+
+        csv.close()
+
+
+
+"""
+GET https://abs.example.com/api/libraries/<ID>/search?<q>
+title -> url encode
+"""
+
+"""
+Update Progress
+PATCH http://abs.example.com/api/me/progress/batch/update
+-d [{},{},{}]
+
+libraryItemId - REQUIRED
+episodeId - For Podcasts
+duration - ?
+isFinished
+currentTime
+progress - auto if finished
+finishedAt - auto if finished
+startedAt - auto if finished
+hideFromContinueListening
+"""    
+
+
+### Book Fingerprint ###
+"""
+mediaType
+media{}
+    metadata{}
+        title
+        subtitle
+        authors[]
+            n{}
+                name
+        narrators[]
+        series[]
+            n{}
+                name
+                sequence
+        publishedYear
+        publisher
+        isbn
+        asin
+        language
+        explicit
+        abridged
+    tags[]
+    Audiofiles[]
+        N{}
+            metadata{}
+                size
+            format
+            duration
+            bitrate
+            language
+            codec
+            timeBase
+            channels
+            channelLayout
+            chapters[]?
+            metaTags{} (I assume these are the embeded tags)
+            mimeType
+"""
+
+
+
+if args.pull or not os.path.isfile("progress.json"):
+    pullProgress()
+
+goodreadsCSV()
---
+++ uploadProgress.py
@@ -0,0 +1,244 @@
+
+import json
+import os
+import shutil
+import sqlite3
+import requests
+import argparse
+from thefuzz import fuzz
+
+# import time, datetime
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-H","--host", action='store',help="ABS host, overwrites hardcoded host.")
+parser.add_argument("-k","--key", action='store',help="API key, overwrites hardcoded key.")
+parser.add_argument("-l","--library", action='store',help="Library ID, overwrites debug library ID.")
+parser.add_argument("-u","--useMissing", action='store_true',help="Whether to use a progress.json or missing.json")
+args = parser.parse_args()
+
+
+# ##### Configuration #####
+
+# Optional hardcoding
+ABS_HOST = "http://localhost:13378"
+API_KEY = ""
+DEBUG_LIBRARY = "0309d2a8-20ff-4ca6-a724-3b8e1831ef40"
+LIBRARY_ID = DEBUG_LIBRARY
+
+if args.host:
+    ABS_HOST = args.host
+
+if args.key:
+    API_KEY = args.key
+
+if args.library:
+    LIBRARY_ID = args.library
+
+ME_URI = "/api/me"
+SEARCH_URI = f"/api/libraries/{LIBRARY_ID}/search"
+BATCH_URI = "/api/me/progress/batch/update"
+
+AUTH = f"?token={API_KEY}"
+
+##### Methods #####
+
+def batchUpload(progs):
+    books = []
+    for p in progs.values():
+        if p["mediaItemType"] != "book":
+            continue
+
+        book={"libraryItemId":p["newLibraryItemId"]}
+        # book["episodeId"]=p["episodeId"]
+
+        book["isFinished"]=p["isFinished"]
+        book["progress"]=p["progress"]
+        book["ebookProgress"]=p["ebookProgress"]
+
+        book["currentTime"]=p["currentTime"]
+        book["finishedAt"]=p["finishedAt"]
+        book["startedAt"]=p["startedAt"]
+
+        book["hideFromContinueListening"]=p["hideFromContinueListening"]
+        
+        books.append(book)
+
+    req = requests.patch(ABS_HOST+BATCH_URI+AUTH,json=books)
+
+def matchHardPath(path, jBook, qBook):
+    keys = path.split("|")
+    jVal = jBook
+    qVal = qBook
+
+    for key in keys:
+        jVal = jVal[key]
+        qVal = qVal[key]
+
+    if jVal == qVal:
+        return qBook["id"]
+
+    return None
+
+###### Main ######
+
+bookJSON = []
+if args.useMissing:
+    with open("missing.json","r") as js:
+        bookJSON = json.load(js)
+        js.close()    
+else:
+    with open("progress.json","r") as js:
+        bookJSON = json.load(js)
+        js.close()
+
+foundBooks = {}
+missingBooks = []
+for jBook in bookJSON:
+    prog = jBook["progress"]
+    prog["newLibraryItemId"] = None
+    
+    resp = requests.get(ABS_HOST+SEARCH_URI+AUTH+f"&")
+
+    # ### High Confidence match if ASIN or ISBN is present ###
+
+    for k in ["asin","isbn"]:
+        val = jBook["media"]["metadata"][k]
+        if val != None:
+            q = requests.get(ABS_HOST+SEARCH_URI+AUTH+f"&q={val}")
+            for qBook in q.json()["book"]:
+                prog["newLibraryItemId"] = matchHardPath(f"media|metadata|{k}",jBook,qBook["libraryItem"])
+                if prog["newLibraryItemId"] != None:
+                    break    
+
+            if prog["newLibraryItemId"] != None:
+                foundBooks.append(prog)
+                continue
+
+
+    ### Fuzzy Matching ###
+
+    title = jBook["media"]["metadata"]["title"]
+    series = jBook["media"]["metadata"]["series"] #array of id,name,sequence
+
+    queriesLikelyToHit = [title]
+    for s in series:
+        queriesLikelyToHit.append(s["name"])
+
+
+    # People data dont show the books they're associated with
+    # for a in authors:
+    #     queriesLikelyToHit.append(a["name"])
+
+    # for n in narrators:
+    #     queriesLikelyToHit.append(n)
+
+    # Chapter titles and Publication Years arent searched
+    # Publisher isnt searched
+    # Descriptions also arent searched 
+    
+
+    matchData = {}
+    for q in queriesLikelyToHit:
+        resp = requests.get(ABS_HOST+SEARCH_URI+AUTH+f"&q={q}")
+        print(f"                          === Searching {q} ===")
+        print()
+        hits = []
+        for k in resp.json()["book"]:
+            hits.append(k["libraryItem"])
+        
+        for s in resp.json()["series"]:
+            print(f"{f"Books in ":>30}{s["series"]["name"]}")
+            hits += s["books"]
+
+
+        for b in hits:
+            newBookId = b["id"]
+            if newBookId in matchData:
+                matchData[newBookId]["count"] += 1
+            else:
+                matchData[newBookId] = b
+                matchData[newBookId]["count"] = 1
+    
+    # Sort list of matching IDs by how often they showed up (presumably the most overlap would be the book we're after)
+    matchList = []
+    for m in matchData:
+        matchList.append({"id":m,"count":matchData[m]["count"]})
+    matchList = sorted(matchList,key=lambda kv: kv["count"], reverse=True)
+
+    matchScores = []
+    for mk in matchList:
+        m = matchData[mk["id"]]
+        if m["mediaType"] != jBook["mediaType"]:
+            continue
+
+        report = {"id":mk["id"]}
+        report["title"] = fuzz.ratio(m["media"]["metadata"]["title"], jBook["media"]["metadata"]["title"])
+        report["title_set"] = fuzz.token_set_ratio(m["media"]["metadata"]["title"], jBook["media"]["metadata"]["title"])
+        report["title_sub_swap"] = fuzz.token_set_ratio(m["media"]["metadata"]["subtitle"], jBook["media"]["metadata"]["title"])
+        # report["sub_swap_string"] = f"{m["media"]["metadata"]["subtitle"]}|{jBook["media"]["metadata"]["title"]}"
+
+        report["pub_year_diff"] = -1000
+        if m["media"]["metadata"]["publishedYear"] != None and jBook["media"]["metadata"]["publishedYear"] != None:
+            report["pub_year_diff"] = -abs(int(jBook["media"]["metadata"]["publishedYear"])-int(m["media"]["metadata"]["publishedYear"]))
+
+        # Books found through the series section dont have authors populated
+        # Instead must fingerprint on narrators, publishedYear, publisher
+        report["narrators"] = []
+        for n in m["media"]["metadata"]["narrators"]:
+            record = {"name":n, "best":0}
+            for jn in jBook["media"]["metadata"]["narrators"]:
+                score = fuzz.token_set_ratio(n, jn)
+                if score>record["best"]:
+                    record["best"] = score
+            report["narrators"].append(record)
+
+        report["authors"] = []
+        for n in m["media"]["metadata"]["authors"]:
+            record = {"name":n["name"], "best":0}
+            for jn in jBook["media"]["metadata"]["authors"]:
+                score = fuzz.token_set_ratio(n["name"], jn["name"])
+                if score>record["best"]:
+                    record["best"] = score
+            report["authors"].append(record)
+        
+        matchScores.append(report)
+    
+
+    sortedScores = sorted(matchScores, key=lambda x: (x["title"], x["title_set"], x["pub_year_diff"]), reverse=True)
+    
+    for r in sortedScores:
+        if len(r["authors"]) > 0:
+            aveAuth = sum([x["best"] for x in r["authors"]]) / len(r["authors"])
+            if aveAuth >= 85:
+                # Probably authors match but are missing abreviations or titles
+                prog["newLibraryItemId"] = r["id"]
+                break
+        
+        if len(r["narrators"]) > 0:
+            aveNarr = sum([x["best"] for x in r["narrators"]]) / len(r["narrators"])
+            if aveNarr >= 85:
+                # Probably narrators match but are missing abreviations or titles
+                prog["newLibraryItemId"] = r["id"]
+                break
+
+    if prog["newLibraryItemId"] != None:
+        if prog["newLibraryItemId"] not in foundBooks:
+            foundBooks[prog["newLibraryItemId"]] = prog
+        else:
+            missingBooks.append(prog)
+            missingBooks.append(foundBooks[prog["newLibraryItemId"]])
+            del foundBooks[prog["newLibraryItemId"]]
+        continue
+    else:
+        missingBooks.append(prog)
+
+batchUpload(foundBooks)
+
+# Convert prog objects back into full book progresses for later reprocessing
+missingIDs = [x["libraryItemId"] for x in missingBooks]
+missingProgs = [x for x in bookJSON if x["id"] in missingIDs]
+
+with open("missing.json","w") as miss:
+    miss.write(json.dumps(missingProgs))
+    miss.close()
